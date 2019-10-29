cec68770f9af3557c712bc4e6abec6e6d6dc204c
Eoan O'Dea <odeaeoan@gmail.com>
2019-10-29 17:00:54 +0000

    Integrated VueJS to laravel

---
+++ server/resources/js/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="main">
+        <header id="header">
+            <h1>
+                Vue JS Component
+            </h1>
+        </header>
+        <div id="content">
+            Some content
+        </div>
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        //
+      }
+    },
+    components: {
+        //
+    }
+  }
+</script>
