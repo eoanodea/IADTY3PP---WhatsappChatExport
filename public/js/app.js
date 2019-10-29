f4f722cc2ef62c22aab668fb98512111fe8cdfd3
Eoan O'Dea <odeaeoan@gmail.com>
2019-10-29 17:08:02 +0000

    moved laravel&vuejs app to root directory

--- public/js/app.js
+++ public/js/app.js
@@ -49497,8 +49497,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/eoanodea/Sites/IADTY3/ProfessionalPractice/IADTY3PPClientMangementSystem/server/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/eoanodea/Sites/IADTY3/ProfessionalPractice/IADTY3PPClientMangementSystem/server/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/eoanodea/Sites/IADTY3/ProfessionalPractice/IADTY3PPClientMangementSystem/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/eoanodea/Sites/IADTY3/ProfessionalPractice/IADTY3PPClientMangementSystem/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
