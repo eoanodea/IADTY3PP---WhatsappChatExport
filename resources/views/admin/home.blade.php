b0d6677fb36fb39c9e058a29ce64b349ef354a47
Eoan O'Dea <odeaeoan@gmail.com>
2019-11-01 17:44:49 +0000

    Updated home.blade.php

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,10 +14,6 @@
                         </div>
                     @endif
                     <div id="app">
-                        {{-- <list-users></list-users>
-                        <passport-clients></passport-clients>
-                        <passport-authorized-clients></passport-authorized-clients>
-                        <passport-personal-access-tokens></passport-personal-access-tokens> --}}
                     </div>
                 </div>
             </div>
