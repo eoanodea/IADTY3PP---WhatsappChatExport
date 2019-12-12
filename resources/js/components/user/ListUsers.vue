02a72a4a8261b10dc1c2133b0e254521fe71c4cd
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-12-12 23:51:07 +0000

    Styling - Clients(Users) Table

--- resources/js/components/user/ListUsers.vue
+++ resources/js/components/user/ListUsers.vue
@@ -1,25 +1,26 @@
 <template>
     <div class="fluid-container">
-        <md-table md-card v-if="users !== null">
+
             <md-table-toolbar>
-                <h1 class="md-title">Clients</h1>
-                <md-button class="md-raised md-primary" to="/admin/users/new">Add Client</md-button>
+                <h1 class="md-title accent">Clients</h1>
+                <md-button class="md-raised md-accent text" to="/admin/users/new">Add Client</md-button>
             </md-table-toolbar>
 
-            <md-table-row>
+          <md-table class="background" md-card v-if="users !== null">
+            <!-- <md-table-row class="background">
                 <md-table-head md-numeric>ID</md-table-head>
                 <md-table-head>Name</md-table-head>
                 <md-table-head>Email</md-table-head>
                 <md-table-head></md-table-head>
-            </md-table-row>
-            <md-table-row v-for="user in users" v-bind:key="user.id" class="item">
-                <md-table-cell md-numeric>{{user.id}}</md-table-cell>
-                <md-table-cell>{{ user.first_name + " " + user.last_name }}</md-table-cell>
-                <md-table-cell>{{ user.email }}</md-table-cell>
-                <md-table-cell>
-                    <md-button :to="'/admin/users/show/' + user.id" class="md-accent">Profile</md-button>
-                    <md-button :to="'/admin/users/edit/' + user.id">Edit</md-button>
-                    <DeleteUser v-bind:id="user.id"/>
+            </md-table-row> -->
+            <md-table-row v-for="user in users" v-bind:key="user.id" class="item background">
+                <!-- <md-table-cell class="background" md-numeric>{{user.id}}</md-table-cell> -->
+                <md-table-cell class="background">{{ user.first_name + " " + user.last_name }}</md-table-cell>
+                <md-table-cell class="background">{{ user.email }}</md-table-cell>
+                <md-table-cell class="background">
+                    <md-button :to="'/admin/users/show/' + user.id" class="md-accent">View Profile</md-button>
+                    <!-- <md-button :to="'/admin/users/edit/' + user.id">Edit</md-button> -->
+                    <!-- <DeleteUser v-bind:id="user.id"/> -->
                 </md-table-cell>
             </md-table-row>
         </md-table>
@@ -56,10 +57,24 @@
 </script>
 <style>
     .item {
-        background: none!important;
+        background: none !important;
     }
 
     .md-title {
+      text-shadow: 1px 1px 2px #000 !important;
+    }
+
+    .background {
+      background-color: #3b3b3b !important;
+      font-size: 18px !important;
+      padding: 10px;
+    }
+
+    .accent {
       color: #00cc83 !important;
+    }
+
+    .text {
+      color: #fff !important;
     }
 </style>
