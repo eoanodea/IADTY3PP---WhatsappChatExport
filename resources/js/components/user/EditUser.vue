f586acd164e437bffd9f2f9373196eb5793bba36
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-09 23:58:34 +0000

    Renovated Edit Client Form

--- resources/js/components/user/EditUser.vue
+++ resources/js/components/user/EditUser.vue
@@ -1,57 +1,58 @@
 <template>
 <div class="fluid-container">
-    <div class="row">
-        <div class="col-6">
-            <form novalidate class="md-layout" method="PUT" @submit.stop.prevent="submitUser">
-                <md-card class="md-layout-item background">
-                    <md-card-header>
-                        <div class="md-title accent">Edit Client</div>
-                    </md-card-header>
-                    <md-card-content>
-                        <p v-if="errors.length">
-                            <b>Please correct the following error(s):</b>
-                            <ul>
-                                <li v-for="error in errors" v-bind:key="error.id">
-                                    {{ error.message }}
-                                </li>
-                            </ul>
-                        </p>
-                        <md-field>
-                            <label class="accent" for="first_name">First Name</label>
-                            <md-input name="first_name" type="text" class="form-control" placeholder="First Name" autocomplete="given-name" v-model="user.first_name" />
-                        </md-field>
-                        <md-field>
-                            <label class="accent" for="last_name">Last Name</label>
-                            <md-input name="last_name" type="text" class="form-control" placeholder="Last Name" autocomplete="family-name" v-model="user.last_name" />
-                        </md-field>
-                        <md-field>
-                            <label class="accent" for="email">Email</label>
-                            <md-input name="email" type="email" class="form-control" placeholder="Email" autocomplete="email" v-model="user.email" /> <br />
-                        </md-field>
-                        <md-field>
-                            <label class="accent" for="mobile">Mobile Number</label>
-                            <md-input name="mobile" type="text" class="form-control" placeholder="Mobile Number" autocomplete="mobile" v-model="user.mobile_number" /> <br />
-                        </md-field>
-                        <md-field>
-                            <label class="accent" for="address">Address</label>
-                            <md-textarea name="address" type="text" class="form-control" placeholder="Address" autocomplete="address" v-model="user.address" /> <br />
-                        </md-field>
-                        <md-field>
-                            <label class="accent" for="password">Password</label>
-                            <md-input type="password" name="password" class="form-control" placeholder="Password" v-model="user.password" /> <br />
-                        </md-field>
-                        <md-field>
-                            <label class="accent" for="confirm_password">Confirm Password</label>
-                            <md-input type="password" name="confirm_password" class="form-control" placeholder="Password Confirmation" v-model="user.confirm_password" /> <br />
-                        </md-field>
-                    </md-card-content>
-                    <md-card-actions>
-                        <md-button type="submit" :disabled="submitting" class="md-primary md-raised saveBtn">Save</md-button>
-                    </md-card-actions>
-                </md-card>
-            </form>
-        </div>
+    <div class="md-layout">
+      <div class="md-layout-item">
+        <md-table-toolbar>
+          <h1 class="md-title accent">Edit Client</h1>
+        </md-table-toolbar>
+      </div>
     </div>
+
+    <form novalidate class="md-layout" method="PUT" @submit.stop.prevent="submitUser">
+        <md-card class="md-layout-item background">
+            <md-card-content>
+                <p v-if="errors.length">
+                    <b>Please correct the following error(s):</b>
+                    <ul>
+                        <li v-for="error in errors" v-bind:key="error.id">
+                            {{ error.message }}
+                        </li>
+                    </ul>
+                </p>
+                <md-field>
+                    <label class="accent" for="first_name">First Name</label>
+                    <md-input name="first_name" type="text" class="form-control" placeholder="First Name" autocomplete="given-name" v-model="user.first_name" />
+                </md-field>
+                <md-field>
+                    <label class="accent" for="last_name">Last Name</label>
+                    <md-input name="last_name" type="text" class="form-control" placeholder="Last Name" autocomplete="family-name" v-model="user.last_name" />
+                </md-field>
+                <md-field>
+                    <label class="accent" for="email">Email</label>
+                    <md-input name="email" type="email" class="form-control" placeholder="Email" autocomplete="email" v-model="user.email" /> <br />
+                </md-field>
+                <md-field>
+                    <label class="accent" for="mobile">Mobile Number</label>
+                    <md-input name="mobile" type="text" class="form-control" placeholder="Mobile Number" autocomplete="mobile" v-model="user.mobile_number" /> <br />
+                </md-field>
+                <md-field>
+                    <label class="accent" for="address">Address</label>
+                    <md-textarea name="address" type="text" class="form-control" placeholder="Address" autocomplete="address" v-model="user.address" /> <br />
+                </md-field>
+                <!-- <md-field>
+                    <label class="accent" for="password">Password</label>
+                    <md-input type="password" name="password" class="form-control" placeholder="Password" v-model="user.password" /> <br />
+                </md-field>
+                <md-field>
+                    <label class="accent" for="confirm_password">Confirm Password</label>
+                    <md-input type="password" name="confirm_password" class="form-control" placeholder="Password Confirmation" v-model="user.confirm_password" /> <br />
+                </md-field> -->
+            </md-card-content>
+            <md-card-actions>
+                <md-button type="submit" :disabled="submitting" class="md-primary md-raised btnAccent">Save</md-button>
+            </md-card-actions>
+        </md-card>
+    </form>
 </div>
 </template>
 <script>
@@ -116,43 +117,43 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.md-field {
-    flex-direction: column;
-}
-.md-input,
-.md-textarea {
-    color: #fff;
-}
+// .md-field {
+//     flex-direction: column;
+// }
+// .md-input,
+// .md-textarea {
+//     color: #fff;
+// }
 
-.row {
-    display: flex;
-    justify-content: center;
-}
+// .row {
+//     display: flex;
+//     justify-content: center;
+// }
 
-.md-title {
-    text-shadow: 1px 1px 2px #000 !important;
-}
+// .md-title {
+//     text-shadow: 1px 1px 2px #000 !important;
+// }
 
-.background {
-    background-color: #3b3b3b !important;
-    font-size: 18px !important;
-    padding: 10px;
-}
+// .background {
+//     background-color: #3b3b3b !important;
+//     font-size: 18px !important;
+//     padding: 10px;
+// }
 
-.saveBtn {
-    color: #fff !important;
-    background-color: #00cc83 !important;
-}
+// .saveBtn {
+//     color: #fff !important;
+//     background-color: #00cc83 !important;
+// }
 
-.accent {
-    color: #00cc83 !important;
-}
+// .accent {
+//     color: #00cc83 !important;
+// }
 
-.subject {
-    color: #818181 !important;
-}
+// .subject {
+//     color: #818181 !important;
+// }
 
-.text {
-    color: #fff !important;
-}
+// .text {
+//     color: #fff !important;
+// }
 </style>
