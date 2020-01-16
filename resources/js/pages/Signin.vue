30106a2f6552a771a20f539edf108519270c40b6
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-16 21:03:04 +0000

    Added error colour

--- resources/js/pages/Signin.vue
+++ resources/js/pages/Signin.vue
@@ -22,9 +22,9 @@
         </md-card-header>
         <md-card-content>
             <p v-if="errors.length">
-                <b>Please correct the following error(s):</b>
+                <b class="error">Please correct the following error(s):</b>
                 <ul>
-                    <li v-for="error in errors" v-bind:key="error.id">
+                    <li v-for="error in errors" v-bind:key="error.id" class="error">
                         {{ error.message }}
                     </li>
                 </ul>
