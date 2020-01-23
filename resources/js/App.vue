5eb938697dc4c8fb3959ce9316439cb972d6bd2d
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-23 20:15:34 +0000

    Updated App Bar with Carbon (Work in Progress)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,13 +1,39 @@
-<!--
-@Author: John Carlo M. Ramos
-@Date:   2019-11-07T18:25:17+00:00
-@Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
-@Last modified by:   John Carlo M. Ramos
-@Last modified time: 2019-11-08T09:05:19+00:00
--->
-
 <template>
- <div class="page-container">
+<div>
+<cv-header aria-label="Carbon header">
+  <cv-skip-to-content href="/admin/home">
+    Skip to Content
+  </cv-skip-to-content>
+  <cv-header-name href="/admin/home" prefix="CLIENTELIGENCE">
+    <router-link to="/admin/home">
+    </router-link>
+  </cv-header-name>
+  <template slot="header-global">
+    <cv-header-global-action
+      aria-label="App switcher"
+      aria-controls="switcher-panel"
+      @click="actionAppSwitcher">
+      <AppSwitcher20 />
+    </cv-header-global-action>
+  </template>
+  <template slot="right-panels" @click="menuVisible = !menuVisible" v-if="areRightPanels && authenticated">
+    <cv-header-panel class="" id="user-panel">
+      <cv-switcher>
+        <cv-switcher-item>
+          <cv-switcher-item-link>
+            <cv-header-name>
+              Good day, {{user.first_name}}
+            </cv-header-name>
+            <NavBar ref="navBarRef" v-on:menu-visible="menuVisible = !menuVisible" />
+          </cv-switcher-item-link>
+        </cv-switcher-item>
+      </cv-switcher>
+    </cv-header-panel>
+  </template>
+</cv-header>
+<router-view style="margin-top:300px;"></router-view>
+</div>
+ <!-- <div class="page-container">
     <md-app md-waterfall md-mode="fixed" class="app-container">
       <md-app-toolbar class="md-primary space-between">
         <router-link to="/admin/home">
@@ -17,7 +43,7 @@
           <md-icon>menu</md-icon>
         </md-button>
         </md-app-toolbar>
-        <md-app-drawer md-right md-persistent="full" :md-active.sync="menuVisible" v-if="authenticated">
+        <md-app-drawer md-right md-persistent="full" :md-active.sync="menuVisible">
           <md-toolbar class="md-transparent" md-elevation="0">Good day, {{user.first_name}}</md-toolbar>
           <NavBar ref="navBarRef" v-on:menu-visible="menuVisible = !menuVisible" />
         </md-app-drawer>
@@ -26,19 +52,62 @@
           <router-view></router-view>
       </md-app-content>
     </md-app>
-  </div>
+  </div> -->
 </template>
 <script>
   import { mapGetters } from 'vuex'
   import Vue from 'vue';
+  import 'carbon-components/css/carbon-components.css';
+  import CarbonComponentsVue from '@carbon/vue/src/index';
+  Vue.use(CarbonComponentsVue);
   import NavBar from './components/NavBar.vue'
-  import {MdApp, MdContent, MdButton, MdToolbar, MdDrawer} from 'vue-material/dist/components'
+  // import {MdApp, MdContent, MdButton, MdToolbar, MdDrawer} from 'vue-material/dist/components'
 
-  Vue.use(MdContent);
-  Vue.use(MdApp);
-  Vue.use(MdToolbar);
-  Vue.use(MdButton);
-  Vue.use(MdDrawer);
+  // Vue.use(MdContent);
+  // Vue.use(MdApp);
+  // Vue.use(MdToolbar);
+  // Vue.use(MdButton);
+  // Vue.use(MdDrawer);
+
+  import {
+  CvHeader,
+  CvHeaderGlobalAction,
+  CvHeaderMenu,
+  CvHeaderMenuButton,
+  CvHeaderMenuItem,
+  CvHeaderName,
+  CvHeaderNav,
+  CvHeaderPanel,
+  CvSideNav,
+  CvSideNavItems,
+  CvSideNavLink,
+  CvSideNavMenu,
+  CvSideNavMenuItem,
+  CvSkipToContent,
+  CvSwitcher,
+  CvSwitcherItem,
+  CvSwitcherItemLink,
+  CvHeaderSideNavItems,
+} from '@carbon/vue/src';
+
+Vue.use(CvHeader);
+Vue.use(CvHeaderGlobalAction);
+Vue.use(CvHeaderMenu);
+Vue.use(CvHeaderMenuButton);
+Vue.use(CvHeaderMenuItem);
+Vue.use(CvHeaderName);
+Vue.use(CvHeaderNav);
+Vue.use(CvHeaderPanel);
+Vue.use(CvSideNav);
+Vue.use(CvSideNavItems);
+Vue.use(CvSideNavLink);
+Vue.use(CvSideNavMenu);
+Vue.use(CvSideNavMenuItem);
+Vue.use(CvSkipToContent);
+Vue.use(CvSwitcher);
+Vue.use(CvSwitcherItem);
+Vue.use(CvSwitcherItemLink);
+Vue.use(CvHeaderSideNavItems);
 
   export default {
     data() {
