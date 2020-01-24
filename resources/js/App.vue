3267f39a48d5e576516aa7a021fab48eb86f3a18
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-24 00:29:05 +0000

    Added Nav Links. Right Panel is Work in Progress

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,37 +1,99 @@
 <template>
 <div>
-<cv-header aria-label="Carbon header">
-  <cv-skip-to-content to="/admin/home">
-    Skip to Content
-  </cv-skip-to-content>
-  <cv-header-name to="/admin/home" prefix="CLIENTELIGENCE">
-    <router-link to="/admin/home">
-    </router-link>
-  </cv-header-name>
-  <template slot="header-global">
-    <cv-header-global-action
-      aria-label="App switcher"
-      aria-controls="switcher-panel"
-      @click="actionAppSwitcher">
-      <AppSwitcher20 />
-    </cv-header-global-action>
-  </template>
-  <template slot="right-panels" @click="menuVisible = !menuVisible" v-if="areRightPanels && authenticated">
-    <cv-header-panel class="" id="user-panel">
-      <cv-switcher>
-        <cv-switcher-item>
-          <cv-switcher-item-link>
-            <cv-header-name>
-              Good day, {{user.first_name}}
-            </cv-header-name>
-            <NavBar ref="navBarRef" v-on:menu-visible="menuVisible = !menuVisible" />
-          </cv-switcher-item-link>
-        </cv-switcher-item>
-      </cv-switcher>
-    </cv-header-panel>
-  </template>
-</cv-header>
-<router-view style="margin-top:300px;"></router-view>
+  <cv-header aria-label="Carbon header">
+    <cv-skip-to-content to="/admin/home">
+      Skip to Content
+    </cv-skip-to-content>
+
+    <cv-header-name to="/admin/home" prefix="CLIENTELIGENCE">
+      <router-link to="/admin/home">
+      </router-link>
+    </cv-header-name>
+
+    <cv-header-nav aria-label="Carbon Nav" @click="menuVisible = !menuVisible" v-if="authenticated">
+      <cv-header-menu-item to="/admin/home">
+        Dashboard
+      </cv-header-menu-item>
+
+      <cv-header-menu-item to="/admin/users" @click="$emit('menu-visible')">
+        Clients
+      </cv-header-menu-item>
+
+      <cv-header-menu-item to="/admin/services" @click="$emit('menu-visible')">
+        Services
+      </cv-header-menu-item>
+
+      <cv-header-menu-item to="/admin/assignments" @click="$emit('menu-visible')">
+        Projects
+      </cv-header-menu-item>
+
+      <cv-header-menu-item to="/admin/about" @click="$emit('menu-visible')">
+        About
+      </cv-header-menu-item>
+
+      <cv-header-menu-item @click="signOut">
+        Sign Out
+      </cv-header-menu-item>
+    </cv-header-nav>
+
+    <template slot="header-global">
+      <cv-header-global-action
+        aria-label="App switcher"
+        aria-controls="switcher-panel"
+        @click="actionAppSwitcher">
+        <AppSwitcher20 />
+      </cv-header-global-action>
+    </template>
+
+    <template slot="right-panels" @click="menuVisible = !menuVisible" v-if="areRightPanels && authenticated">
+      <cv-header-panel class="" id="switcher-panel">
+        <cv-header-name>
+          Good day, {{user.first_name}}
+        </cv-header-name>
+
+        <cv-switcher>
+          <cv-switcher-item>
+            <cv-switcher-item-link to="/admin/home" @click="$emit('menu-visible')">
+              <!-- <NavBar ref="navBarRef" v-on:menu-visible="menuVisible = !menuVisible" /> -->
+              Dashboard
+            </cv-switcher-item-link>
+          </cv-switcher-item>
+
+          <cv-switcher-item>
+            <cv-switcher-item-link to="/admin/users" @click="$emit('menu-visible')">
+              Clients
+            </cv-switcher-item-link>
+          </cv-switcher-item>
+          <cv-switcher-item>
+
+            <cv-switcher-item-link to="/admin/services" @click="$emit('menu-visible')">
+              Services
+            </cv-switcher-item-link>
+          </cv-switcher-item>
+
+          <cv-switcher-item>
+            <cv-switcher-item-link to="/admin/assignments" @click="$emit('menu-visible')">
+              Projects
+            </cv-switcher-item-link>
+          </cv-switcher-item>
+
+          <cv-switcher-item>
+            <cv-switcher-item-link to="/admin/about" @click="$emit('menu-visible')">
+              About
+            </cv-switcher-item-link>
+          </cv-switcher-item>
+
+          <cv-switcher-item>
+            <cv-switcher-item-link @click="signOut">
+              Sign Out
+            </cv-switcher-item-link>
+          </cv-switcher-item>
+
+        </cv-switcher>
+      </cv-header-panel>
+    </template>
+  </cv-header>
+  <router-view style="margin-top:300px;"></router-view>
 </div>
  <!-- <div class="page-container">
     <md-app md-waterfall md-mode="fixed" class="app-container">
@@ -61,6 +123,7 @@
   import CarbonComponentsVue from '@carbon/vue/src/index';
   Vue.use(CarbonComponentsVue);
   import NavBar from './components/NavBar.vue'
+  import { mapActions } from 'vuex' 
   // import {MdApp, MdContent, MdButton, MdToolbar, MdDrawer} from 'vue-material/dist/components'
 
   // Vue.use(MdContent);
@@ -89,6 +152,7 @@
   CvSwitcherItemLink,
   CvHeaderSideNavItems,
 } from '@carbon/vue/src';
+import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
 
 Vue.use(CvHeader);
 Vue.use(CvHeaderGlobalAction);
@@ -108,26 +172,66 @@ Vue.use(CvSwitcher);
 Vue.use(CvSwitcherItem);
 Vue.use(CvSwitcherItemLink);
 Vue.use(CvHeaderSideNavItems);
+Vue.use(AppSwitcher20);
 
   export default {
     data() {
       return {
-        menuVisible: false
+        menuVisible: false,
       }
     },
     components: {
-      NavBar
+      // NavBar,
+      CvHeader,
+      CvHeaderPanel,
+      CvHeaderName,
+      CvHeaderNav,
+      CvHeaderSideNavItems,
+      CvHeaderGlobalAction,
+      CvHeaderMenu,
+      CvHeaderMenuItem,
+      CvHeaderMenuButton,
+      CvSideNav,
+      CvSideNavLink,
+      CvSideNavMenu,
+      CvSideNavItems,
+      CvSideNavMenuItem,
+      CvSkipToContent,
+      CvSwitcher,
+      CvSwitcherItem,
+      CvSwitcherItemLink,
+      AppSwitcher20
     },
     computed: {
       ...mapGetters({
           authenticated: 'auth/authenticated',
           user: 'auth/user'
-      })
+      }),
+      areRightPanels() {
+        // return settings.group.rightPanels.length > 0;
+      },
     },
     methods: {
       click: function() {
         this.menuVisible = !this.menuVisible
-      }
+      },
+      ...mapActions({
+        signOutAction: 'auth/signOut'
+      }),
+      signOut() {
+        this.signOutAction().then(() => {
+          this.$emit('menu-visible')
+          this.$router.replace({
+            name: 'signin'
+          })
+        })
+      },
+      actionAppSwitcher() {
+        this.doActionSwitcher();
+      },
+    },
+    mounted() {
+      this.doActionSwitcher = () => action('Notifications - click');
     }
   }
 </script>
