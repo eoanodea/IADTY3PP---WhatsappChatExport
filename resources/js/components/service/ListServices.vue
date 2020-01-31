6bb5b7b9a6a92999d8e2c4786febcf2fe6e083c5
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-31 19:33:01 +0000

    Added pagination for list service and default tasks (Not Wrking)

--- resources/js/components/service/ListServices.vue
+++ resources/js/components/service/ListServices.vue
@@ -82,6 +82,61 @@
         </div>
     </div>
 
+    <!-- Pagination -->
+    <div class="bx--pagination" data-pagination> <!-- Open Pagination -->
+        <!-- Left Pagination -->
+        <div class="bx--pagination__left">
+        <label id="select-id-pagination-count-label" class="bx--pagination__text" for="select-id-pagination-count">
+            Items per page:
+        </label>
+
+        <div class="bx--select bx--select--inline bx--select__item-count">
+            <select class="bx--select-input" id="select-id-pagination-count" aria-label="select number of items per page" tabindex="0" data-items-per-page>
+            <option class="bx--select-option" value="10" selected>10</option>
+            <option class="bx--select-option" value="20">20</option>
+            <option class="bx--select-option" value="30">30</option>
+            <option class="bx--select-option" value="40">40</option>
+            <option class="bx--select-option" value="50">50</option>
+            </select>
+            <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--select__arrow" width="10" height="6" viewBox="0 0 10 6" aria-hidden="true"><path d="M5 6L0 1 .7.3 5 4.6 9.3.3l.7.7z"></path></svg>
+        </div>
+        <span class="bx--pagination__text">
+            <span data-displayed-item-range>
+            1-10
+            </span> of
+            <span ata-total-items>
+            50
+            </span> items
+        </span>
+        </div>
+
+        <!-- Right Pagination -->
+        <div class="bx--pagination__right">
+        <div class="bx--select bx--select--inline bx--select__page-number">
+            <select class="bx--select-input" id="select-id-pagination-page" aria-label="select page number to view" tabindex="0" data-page-number-input>
+            <option class="bx--select-option" value="1" selected>1</option>
+            <option class="bx--select-option" value="2">2</option>
+            <option class="bx--select-option" value="3">3</option>
+            <option class="bx--select-option" value="4">4</option>
+            <option class="bx--select-option" value="5">5</option>
+            </select>
+            <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--select__arrow" width="10" height="6" viewBox="0 0 10 6" aria-hidden="true"><path d="M5 6L0 1 .7.3 5 4.6 9.3.3l.7.7z"></path></svg>
+        </div>
+        
+        <label id="select-id-pagination-page-label" class="bx--pagination__text" for="select-id-pagination-page">
+            of 5 pages
+        </label>
+        
+        <button class="bx--pagination__button bx--pagination__button--backward" tabindex="0" data-page-backward aria-label="Backward button">
+            <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--pagination__nav-arrow" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M19 23l-8-7 8-7v14z"></path></svg>
+        </button>
+
+        <button class="bx--pagination__button bx--pagination__button--forward" tabindex="0" data-page-forward aria-label="Forward button">
+            <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--pagination__nav-arrow" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M13 9l8 7-8 7V9z"></path></svg>
+        </button>
+        </div>
+    </div> <!-- Close Pagination -->
+
 
         <!-- <md-table-toolbar>
             <h1 class="md-title accent">Services</h1>
