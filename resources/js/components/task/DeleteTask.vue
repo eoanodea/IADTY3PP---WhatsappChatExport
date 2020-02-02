53cbd4f5866e699b6fbf77d132e1d938d69e7185
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-02 15:31:11 +0000

    Redesigned Default Task Delete Modal

--- resources/js/components/task/DeleteTask.vue
+++ resources/js/components/task/DeleteTask.vue
@@ -1,23 +1,54 @@
 <template> 
-    <div>
-        <md-button class="md-secondary md-raised btnDanger" @click="showDialog = true">Delete Default Task</md-button>
+<div>
+    <!-- Modal Button -->
+    <button
+        class="bx--btn bx--btn--lg bx--btn--danger" 
+        type="button" 
+        data-modal-target="#modal-ptxyfo5520i"
+        @click="showDialog = true">
+            Delete Default Task
+    </button>
+
+    <!-- Modal -->
+    <cv-modal
+        kind="danger"
+        :visible="showDialog"
+        @modal-shown="showDialog = true"
+        @modal-hidden="showDialog = false"
+        @modal-hide-request="showDialog = false"
+        @secondary-click="actionSecondary"
+        @primary-click="actionPrimary">
+
+        <template slot="title">WARNING</template>
+        <template slot="content"><p>Are you sure you want to delete this default task?</p></template>
+    
+        <!-- Buttons -->
+        <template slot="secondary-button">No</template>
+        <template slot="primary-button">Yes</template>
+    </cv-modal>
+
+        <!-- <md-button class="md-secondary md-raised btnDanger" @click="showDialog = true">Delete Default Task</md-button>
         <md-dialog :md-active.sync="showDialog">
             <md-dialog-title>Are you sure you want to delete this task?</md-dialog-title>
             <md-dialog-actions>
                 <md-button class="md-secondary md-raised backBtn" @click="showDialog = false">Back</md-button>
                 <md-button class="md-primary md-raised btnDanger" @click="deleteTask">Confirm</md-button>
             </md-dialog-actions>
-        </md-dialog>
-    </div>
+        </md-dialog> -->
+</div>
 </template>
 <script>
     import Vue from 'vue'
-    import {MdDialog, MdButton} from 'vue-material/dist/components'
+    // import {MdDialog, MdButton} from 'vue-material/dist/components'
     import axios from 'axios'
     import router from './../../router'
+    import 'carbon-components/css/carbon-components.css';
+    import CarbonComponentsVue from '@carbon/vue/src/index';
+    import { Modal } from 'carbon-components';
+    import { CvModal } from '@carbon/vue/src';
 
-    Vue.use(MdDialog)
-    Vue.use(MdButton)
+    // Vue.use(MdDialog)
+    // Vue.use(MdButton)
 
     export default {
         props: ['id', 'isActive'],
@@ -30,7 +61,7 @@
             }
         },
         methods: {
-            deleteTask: function() {
+            actionPrimary: function() {
                 const url = this.active === false
                     ? 'task'
                     : 'task/active'
@@ -41,10 +72,14 @@
                 })
             }
         },
+        components: {
+            Modal,
+            CvModal
+        }
     }
 </script>
 <style>
-    .backBtn {
+    /* .backBtn {
       color: #fff !important;
       background-color: #24b6f7 !important;
     }
@@ -52,5 +87,5 @@
     .delBtn {
       color: #fff !important;
       background-color: #ee4444 !important;
-    }
+    } */
 </style>
