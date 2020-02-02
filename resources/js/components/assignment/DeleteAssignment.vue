caa85c1a1e8379f4555e3f2114fa7b20dd044b58
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-02 17:47:13 +0000

    Redesigned Delte Project Modal

--- resources/js/components/assignment/DeleteAssignment.vue
+++ resources/js/components/assignment/DeleteAssignment.vue
@@ -1,23 +1,57 @@
 <template> 
-    <div>
-        <md-button class="md-secondary md-raised btnDanger" @click="showDialog = true">Delete Project</md-button>
+<div>
+    <!-- Modal Button -->
+    <button
+        class="bx--btn bx--btn--lg bx--btn--danger" 
+        type="button" 
+        data-modal-target="#modal-ptxyfo5520i"
+        @click="showDialog = true">
+            Delete Project
+    </button>
+
+    <!-- Modal -->
+    <cv-modal
+        kind="danger"
+        :visible="showDialog"
+        @modal-shown="showDialog = true"
+        @modal-hidden="showDialog = false"
+        @modal-hide-request="showDialog = false"
+        @secondary-click="actionSecondary"
+        @primary-click="actionPrimary">
+
+        <template slot="title">WARNING</template>
+        <template slot="content"><p>Are you sure you want to delete this project?</p></template>
+
+        <!-- Buttons -->
+        <template slot="secondary-button">No</template>
+        <template slot="primary-button">Yes</template>
+    </cv-modal>
+
+
+
+        <!-- <md-button class="md-secondary md-raised btnDanger" @click="showDialog = true">Delete Project</md-button>
         <md-dialog :md-active.sync="showDialog">
             <md-dialog-title>Are you sure you want to delete this assignment?</md-dialog-title>
             <md-dialog-actions>
                 <md-button class="md-secondary md-raised backBtn" @click="showDialog = false">Back</md-button>
                 <md-button class="md-primary md-raised btnDanger" @click="deleteAssignment">Confirm</md-button>
             </md-dialog-actions>
-        </md-dialog>
-    </div>
+        </md-dialog> -->
+</div>
 </template>
+
 <script>
     import Vue from 'vue'
-    import {MdDialog, MdButton} from 'vue-material/dist/components'
+    // import {MdDialog, MdButton} from 'vue-material/dist/components'
     import axios from 'axios'
     import router from './../../router'
+    import 'carbon-components/css/carbon-components.css';
+    import CarbonComponentsVue from '@carbon/vue/src/index';
+    import { Modal } from 'carbon-components';
+    import { CvModal } from '@carbon/vue/src';
 
-    Vue.use(MdDialog)
-    Vue.use(MdButton)
+    // Vue.use(MdDialog)
+    // Vue.use(MdButton)
 
     export default {
         props: ['id'],
@@ -27,7 +61,7 @@
             }
         },
         methods: {
-            deleteAssignment: function() {
+            actionPrimary: function() {
                 axios.delete(`/api/assignment/${this.id}`)
                 .then(response => {
                     if(!response.data) console.log("Error: ", response)
@@ -35,10 +69,14 @@
                 })
             }
         },
+        components: {
+            Modal,
+            CvModal
+        }
     }
 </script>
 <style>
-    .backBtn {
+    /* .backBtn {
       color: #fff !important;
       background-color: #24b6f7 !important;
     }
@@ -46,5 +84,5 @@
     .delBtn {
       color: #fff !important;
       background-color: #ee4444 !important;
-    }
+    } */
 </style>
