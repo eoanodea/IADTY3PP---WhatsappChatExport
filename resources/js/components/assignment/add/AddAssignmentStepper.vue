a9ffb762eb0279fd3547f0cd93975e5693335a11
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-05 10:19:20 +0000

    Played around with Progression Indicator FOR FIVE FUCKING STRAIGHT DAYS NOW AND STILL NO RESULT so I'm scrapping it and replacing with new component

--- resources/js/components/assignment/add/AddAssignmentStepper.vue
+++ resources/js/components/assignment/add/AddAssignmentStepper.vue
@@ -1,6 +1,44 @@
 <template>
-    <div>
-        <md-steppers :md-active-step.sync="active" md-linear>
+<div class="bx--grid" style="padding: 60px 250px;">
+    <!-- Indicator Container -->
+    <ul data-progress data-progress-current class="bx--progress ">
+        <!-- Client and Service -->
+        <li id="first" class="bx--progress-step bx--progress-step--current">
+            <svg><path d="M 7, 7 m -7, 0 a 7,7 0 1,0 14,0 a 7,7 0 1,0 -14,0" ></path></svg>
+            <p tabindex="0" class="bx--progress-label" aria-describedby="label-tooltipfvsw9xwdq7">
+                Client & Service
+            </p>
+            <div id="label-tooltip" role="tooltip" data-floating-menu-direction="bottom" class="bx--tooltip" data-avoid-focus-on-open>
+                <span class="bx--tooltip__caret"></span>
+                <p class="bx--tooltip__text">Client & Service</p>
+            </div>
+            <p class="bx--progress-optional">Choose a Client and Service for your new Project.</p>
+		    <span class="bx--progress-line"></span>
+            
+            <!-- <AddClientAndService v-on:selected-service-client="handleServiceAndClientId"/> -->
+        </li>
+
+        <!-- Assignment Details -->
+        <li id="second" class="bx--progress-step bx--progress-step--incomplete">
+            <svg><path d="M8 1C4.1 1 1 4.1 1 8s3.1 7 7 7 7-3.1 7-7-3.1-7-7-7zm0 13c-3.3 0-6-2.7-6-6s2.7-6 6-6 6 2.7 6 6-2.7 6-6 6z"></path></svg>
+            <p tabindex="0" class="bx--progress-label" aria-describedby="label-tooltipd88ssqwl1mc">
+                Project Details
+            </p>
+            <div id="label-tooltipd88ssqwl1mc" role="tooltip" data-floating-menu-direction="bottom" class="bx--tooltip" data-avoid-focus-on-open>
+                <span class="bx--tooltip__caret"></span>
+			    <p class="bx--tooltip__text">Project Details</p>
+            </div>
+            <p class="bx--progress-optional">Enter Details about the Project.</p>
+		    <span class="bx--progress-line"></span>
+
+            <!-- <AddAssignmentDetails v-on:selected-assignment="handleAssignmentDetails"/> -->
+        </li>
+    </ul>
+
+
+
+
+        <!-- <md-steppers :md-active-step.sync="active" md-linear>
             <md-step id="first" md-label="Client & Service" md-description="Choose a Client and Service for your new Project." :md-done.sync="first">
                 <AddClientAndService v-on:selected-service-client="handleServiceAndClientId"/>
             </md-step>
@@ -10,19 +48,25 @@
             <md-step id="third" md-label="Add Tasks" md-description="Add Tasks to the new Project" :md-done.sync="third">
                 <AddTasksToNewAssignment v-if="serviceId" v-bind:serviceId="serviceId" v-on:selected-tasks="addTasksToAssignment"/>
             </md-step>
-        </md-steppers>
-    </div>
+        </md-steppers> -->
+</div>
 </template>
+
 <script>
     import axios from 'axios';
     import Vue from 'vue';
     import router from './../../../router'
-    import {MdSteppers} from 'vue-material/dist/components'
+    // import {MdSteppers} from 'vue-material/dist/components'
     import AddClientAndService from './AddClientAndService.vue';
     import AddAssignmentDetails from './AddAssignmentDetails'
     import AddTasksToNewAssignment from './AddTasksToNewAssignment';
+    import 'carbon-components/css/carbon-components.css';
+    import CarbonComponentsVue from '@carbon/vue/src/index';
+    import { ProgressIndicator } from 'carbon-components';
+    import { CvProgress, CvProgressStep } from '@carbon/vue/src'
 
-    Vue.use(MdSteppers)
+    Vue.use(CarbonComponentsVue);
+    // Vue.use(MdSteppers)
 
     export default {
         data() {
@@ -80,7 +124,16 @@
         components: {
             AddClientAndService,
             AddAssignmentDetails,
-            AddTasksToNewAssignment
+            AddTasksToNewAssignment,
+            ProgressIndicator,
+            CvProgress, 
+            CvProgressStep
         }
     }
 </script>
+
+<style>
+.bx--progress-line {
+	width: 100%;
+}
+</style>
