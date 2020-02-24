e21e3ecde89430bc2188486a2b65ba5c2b24fb59
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-24 20:38:35 +0000

    Code cleaup and improvements to user dashboard, and task loading indicator

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,15 +1,25 @@
 <template>
-<div class="bx--grid" style="padding: 40px 0px;">
+<div class="bx--grid" style="margin: 80px 0px;">
       <!-- Dashboard: <p v-if="user">{{msg}}, {{ user.first_name }}</p> -->
   <div class="bx--row">
+
+
     <!-- Client Details (Notification Card)-->
     <div class="bx--col-lg-6">
+
+      <!-- Dropdown to choose Project -->
+      <cv-dropdown name="assignment" id="assignment" class="cv-dropdown" v-model="assignmentId" placeholder="Select a Project" >
+          <cv-dropdown-item v-for="assignment in assignments" v-bind:key="assignment.id" :value="assignment.id.toString()">
+              {{assignment.title}}
+          </cv-dropdown-item>
+      </cv-dropdown>
+
       <div data-notification class="bx--inline-notification bx--inline-notification--info" role="alert">
         <div class="bx--inline-notification__details">
           <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--inline-notification__icon" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 5a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 7zm4 17.12h-8v-2.24h2.88v-6.76H13v-2.24h4.13v9H20z"></path></svg>
           
           <!-- Client Details Contents -->
-          <div class="bx--row">
+          <!-- <div class="bx--row"> -->
             <div class="bx--inline-notification__text-wrapper">
               <div class="bx--col-lg-12">
                 <p class="bx--inline-notification__title">{{ user.first_name }} {{ user.last_name }}</p>
@@ -20,31 +30,34 @@
             </div>
           </div>
         </div>
+
+        
+      <!-- Active Task Datatable -->
+      <ListTask v-bind:parentId="assignmentId" v-bind:isActive="true" />
+
+      </div>
+
+      
+        <br />
         <div class="assignment-container" v-if="assignmentId">
-            <!-- Dropdown to choose Project -->
-          <cv-dropdown name="assignment" id="assignment" class="cv-dropdown" v-model="assignmentId" placeholder="Select a Project" >
-              <cv-dropdown-item v-for="assignment in assignments" v-bind:key="assignment.id" :value="assignment.id.toString()">
-                  {{assignment.title}}
-              </cv-dropdown-item>
-          </cv-dropdown>
+         
 
             <!-- Progression Donut -->
-            <div class="bx--row">
+            <div>
                 <Chart v-bind:parentId="assignmentId" style="padding-left:100px;"/>
             </div>
           </div>
 
-          <!-- Active Task Datatable -->
-          <div class="bx--col-lg-6">
-            <ListTask v-bind:parentId="assignmentId" v-bind:isActive="true" />
+         
+          <div>
+           
           </div>
-        </div>
+
             <!-- Comments -->
-        <div class="bx--row">
-          <div class="bx--col-lg-6">
+        
             <CommentTile v-bind:id="assignmentId" v-bind:isActive="true" />
-          </div>
-        </div>
+        
+
       </div>
   </div>
 
