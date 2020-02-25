1ac5e6f19f8c457bde90096ea117b20bda8c7e8d
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-25 13:01:01 +0000

    Fixed bug with Chart Component

--- resources/js/components/user/components/Chart.vue
+++ resources/js/components/user/components/Chart.vue
@@ -24,7 +24,7 @@ export default {
             serviceId: this.parentId,
             data: {
                 labels :[
-                    ''
+                    // ''
                 ],
                 datasets: [
                     {
@@ -56,32 +56,22 @@ export default {
     methods: {
         fetchTasks() {
             this.fetched = false
-            console.log('fetching tasks with ', this.serviceId)
             const url ="task/active"
-            console.log("going ", url);
-            axios.get(`/api/${url}/by/${this.serviceId}`).then(response => {
-            if (response.data.status !== "success") {
-                console.log("error ", response);
-            } else {
-                const tasks = response.data.task;
-                console.log('response!', tasks) 
-                if(tasks.length > 0) {
-                    tasks.map(task => {
-                        const dataObj = {
-                            label: task.title,
-                            data: [
-                                Math.floor( task.percent_done )
-                            ]
-                        }
-                        console.log('running loop!')
-                        this.data.labels[0] = task.title
-                        this.data.datasets[0] = dataObj
 
-                        this.fetched = true
-                    })
-                } else console.log('no tasks!')
-                // this.tasks = 
-            }
+            axios.get(`/api/${url}/by/${this.serviceId}`).then(response => {
+                if (response.data.status !== "success") {
+                    console.log("error ", response);
+                } else {
+                    const tasks = response.data.task;
+                    
+                    if(tasks.length > 0) {
+                        tasks.map(task => {
+                            this.data.datasets[0].data.push(task.percent_done)
+                            this.data.labels.push(task.title)
+                            this.fetched = true
+                        })
+                    } else console.log('no tasks')
+                }
             });
         }
     },
@@ -89,6 +79,9 @@ export default {
             //Watch the serviceId Prop for changes, on change 
             //fetch new data
             parentId: function(newVal, oldVal) {
+                this.data.datasets[0].data = []
+                this.data.labels = []
+
                 this.serviceId = newVal
                 console.log('running!', oldVal, newVal)
                 this.fetchTasks()
