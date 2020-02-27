ed5e90915177d51096037ee4d5022809adbe24d9
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-27 16:13:22 +0000

    Minor Clean up

--- resources/js/components/assignment/Assignment.vue
+++ resources/js/components/assignment/Assignment.vue
@@ -55,53 +55,6 @@
             <CommentTile v-bind:id="assignment.id" v-bind:isAssignment="true" />
         </div>
     </div>
-
-
-
-
-
-
-    <!-- Project Details -->
-    <!-- <div class="md-layout">
-      <div class="md-layout-item">
-        <md-table-toolbar>
-          <h1 class="md-title accent">Project Details</h1>
-          <md-button class="btnWarning" :to="'/checkout/' + assignment.id" v-if="paymentDue() > 0">Pay bill</md-button>
-          <md-button class="btnWarning" :to="'/admin/assignments/edit/' + assignment.id">Edit Details</md-button>
-        </md-table-toolbar>
-      </div>
-    </div>
-
-    <div class="md-layout spacing">
-        <div class="md-layout-item md-medium-size-100 md-small-size-100 md-xsmall-size-100">
-            <md-card class="md-elevation-10 background">
-                <div class="md-layout-item md-medium-size-100 md-small-size-100 md-xsmall-size-100">
-                    <md-card-header class="accent md-display-3">{{ assignment.title }}</md-card-header>
-                    <md-card-header class="text md-display-1"><span class="subject">Deadline: </span>{{assignment.deadline}}</md-card-header>
-                    <md-card-header class="text md-display-1"><span class="subject">Service: </span>{{service.title}}</md-card-header>
-                    <md-card-header class="subject md-headline">
-                        Discount: {{assignment.discount}}%
-                        <br/>
-                        Deposit: {{assignment.deposit}}%
-                        <br/>
-                        Total Price: €{{assignment.total_price}}
-                        <br/>
-                        Completed: {{assignment.completed}}
-                        <br/>
-                        Date of Completeion: {{assignment.date_of_completion}}
-                        <br />
-                        Amount Paid: €{{assignment.amount_paid}}
-                        <br />
-                        Amount Due: €{{paymentDue()}}
-                    </md-card-header>
-                </div>
-            </md-card>
-        </div>
-    </div>
-
-    <div>
-        <ListTask v-bind:parentId="assignment.id" v-bind:isActive="true" />
-    </div> -->
 </div>
 </template>
 
