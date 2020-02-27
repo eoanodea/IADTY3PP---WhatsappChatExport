dfdc5198462dd57d90b70d55e17a26ea327919cc
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-27 20:10:06 +0000

    Updated AddService for Pagination (WORK IN PROGRESS)

--- resources/js/components/service/AddService.vue
+++ resources/js/components/service/AddService.vue
@@ -115,72 +115,25 @@
                 </ul>
             </p>
         </div>
-
-
-
-        <!-- <div class="md-layout">
-            <div class="md-layout-item">
-                <md-table-toolbar>
-                    <h1 class="md-title accent">Add Service</h1>
-                </md-table-toolbar>
-            </div>
-        </div>
-
-        <form novalidate class="md-layout" method="POST" @submit.stop.prevent="validateService">
-            <md-card class="md-layout-item background">
-                <md-card-content>
-                    <p v-if="errors.length">
-                        <b class="error">Please correct the following error(s):</b>
-                        <ul>
-                        <li v-for="error in errors" v-bind:key="error.id" class="error">
-                            {{ error.message }}
-                            </li>
-                        </ul>
-                    </p>
-                    <md-field>
-                        <label class="accent" for="title">Title</label>
-                        <md-input name="title" type="text" class="form-control" placeholder="Title" v-model="service.title" /> <br />
-                    </md-field>
-                    <md-field>
-                        <label class="accent" for="description">Description</label>
-                        <md-input name="description" type="text" class="form-control" placeholder="Description" v-model="service.description" /> <br />
-                    </md-field>
-                    <md-field>
-                        <span class="md-prefix">€</span>
-                        <label class="accent" for="standard_price">Standard Price</label>
-                        <md-input name="standard_price" type="number" class="form-control" placeholder="Standard Price" v-model="service.standard_price" /> <br />
-                    </md-field>
-                    <md-checkbox name="recurring_payment" v-model="service.recurring_payment">Recuring Payment</md-checkbox> <br />
-                    <md-checkbox name="is_public" v-model="service.is_public">Public Service</md-checkbox>
-                </md-card-content>
-                <md-card-actions> -->
-                    <!-- <md-button class="md-secondary md-raised">Back</md-button> -->
-                    <!-- <md-button type="submit" :disabled="submitting" class="md-primary md-raised btnAccent">Save</md-button>
-                </md-card-actions>
-            </md-card>
-        </form> -->
     </div>
 </template>
+
 <script>
     import axios from 'axios';
     import Vue from 'vue';
     import router from './../../router'
-    import 'carbon-components/css/carbon-components.css';
-    import CarbonComponentsVue from '@carbon/vue/src/index';
+    import DataError from './../table/DataError'
+    import { mapGetters } from 'vuex';
+    // import 'carbon-components/css/carbon-components.css';
+    // import CarbonComponentsVue from '@carbon/vue/src/index';
     import { CvCheckbox } from '@carbon/vue/src'
-    // import {MdButton, MdField, MdCard, MdCheckbox} from 'vue-material/dist/components'
 
-    Vue.use(CarbonComponentsVue);
-
-    // Vue.use(MdButton)
-    // Vue.use(MdField)
-    // Vue.use(MdCard)
-    // Vue.use(MdCheckbox)
+    // Vue.use(CarbonComponentsVue);
 
     export default {
         data() {
             return {
-                service: {
+                newService: {
                     title: '',
                     description: '',
                     recurring_payment: false,
@@ -193,49 +146,77 @@
         },
         methods: {
             validateService: function() {
-                const {service} = this
+                const {newService} = this
                 if(
-                    service.title
-                    && service.description
-                    && service.recurring_payment
-                    && service.standard_price
-                    && service.is_public) {
+                    newService.title &&
+                    newService.description &&
+                    newService.recurring_payment &&
+                    newService.standard_price &&
+                    newService.is_public) {
                     this.submitService()
                 }
 
                 this.errors = [];
-                if(!service.title) {
-                     this.errors.push({id: 0, message: 'Title required.'});
+                if(!newService.title) {
+                    this.errors.push({
+                        id: 0, 
+                        message: 'Title required.'
+                        });
                 }
-                if(!service.description) {
-                     this.errors.push({id: 1, message: 'Description required.'});
+                if(!newService.description) {
+                    this.errors.push({
+                        id: 1, message: 'Description required.'
+                        });
                 }
-                if(!service.recurring_payment) {
-                     this.errors.push({id: 2, message: 'Input required.'});
+                if(!newService.recurring_payment) {
+                    this.errors.push({
+                        id: 2, message: 'Input required.'
+                        });
                 }
-                if(!service.standard_price) {
-                     this.errors.push({id: 3, message: 'Standard Price required.'});
+                if(!newService.standard_price) {
+                    this.errors.push({
+                        id: 3, message: 'Standard Price required.'
+                        });
                 }
-                if(!service.is_public) {
-                     this.errors.push({id: 4, message: 'Input required.'});
+                if(!newService.is_public) {
+                    this.errors.push({
+                        id: 4, message: 'Input required.'
+                        });
                 }
             },
             submitService: function() {
-                this.submitting = true
-                const payload = this.service
-               axios.post('/api/service/new', payload)
-                .then(response => {
-                    if(!response.data) {
-                        console.log("Error!", response)
-                        this.errors.push({id: 0, message: JSON.stringify(response.message)})
-                        this.submitting = false
-                    } else router.push({path: `/admin/services/show/${response.data.service.id}`})
+                let {submitting} = this
+                submitting = true
+                this.$store.dispatch('service/addService', this.newService)
+                .then(function(response) {
+                    submitting = false
+                    router.push({
+                        path: `/admin/services/show/${response}`
+                    })
+                }).catch(function(error) {
+                    submitting = false
                 })
+                // this.submitting = true
+                // const payload = this.service
+                // axios.post('/api/service/new', payload)
+                // .then(response => {
+                //     if(!response.data) {
+                //         console.log("Error!", response)
+                //         this.errors.push({id: 0, message: JSON.stringify(response.message)})
+                //         this.submitting = false
+                //     } else router.push({path: `/admin/services/show/${response.data.service.id}`})
+                // })
             }
-
         },
         components: {
-            CvCheckbox
+            CvCheckbox,
+            DataError
+        },
+        computed: {
+            ...mapGetters({
+                error: 'service/error',
+                service: 'service/service'
+            })
         }
     }
 </script>
--- resources/js/components/service/Service.vue
+++ resources/js/components/service/Service.vue
@@ -37,53 +37,6 @@
             <ListTask v-bind:parentId="service.id" v-bind:isActive="false" />
         </div>
     </div>
-
-
-
-
-
-
-    <!-- Service Details -->
-    <!-- <div class="md-layout">
-      <div class="md-layout-item">
-        <md-table-toolbar>
-          <h1 class="md-title accent">Service Details</h1>
-          <md-button class="btnWarning" :to="'/admin/services/edit/' + service.id">Edit Details</md-button>
-        </md-table-toolbar>
-      </div>
-    </div>
-
-    <div class="md-layout spacing">
-        <div class="md-layout-item md-medium-size-100 md-small-size-100 md-xsmall-size-100">
-            <md-card md-with-hover class="md-elevation-10 background">
-                <md-ripple>
-                    <div class="md-layout-item md-medium-size-100 md-small-size-100 md-xsmall-size-100">
-                        <md-card-header class="accent md-display-3">{{ service.title }}</md-card-header>
-                        <md-card-header class="text md-display-1">{{ service.description }}</md-card-header>
-                        <md-card-header class="subject md-headline">
-                            Standard Price: {{service.standard_price}}
-                            <br/>
-                            Recurring Payment? {{service.recurring_payment}}
-                            <br/>
-                            Public Service {{service.is_public = 0 ? "False" : "True"}}
-                        </md-card-header>
-                    </div>
-                </md-ripple>
-            </md-card>
-        </div>
-    </div> -->
-
-    <!-- Default Tasks Details -->
-    <!-- <div> -->
-        <!-- <md-button :to="`/admin/tasks/${false}/` + service.id">Tasks</md-button> -->
-        <!-- <ListTask v-bind:id="service.id" v-bind:isActive="false" /> -->
-    <!-- </div> -->
-
-    <!-- Delete Service Button -->
-    <!-- <md-card-actions>
-        <DeleteService v-bind:id="service.id"/>
-    </md-card-actions> -->
-    
 </div>
 </template>
 
@@ -98,31 +51,6 @@
     import { Modal, DataTable, Loading } from 'carbon-components';
 
     Vue.use(CarbonComponentsVue);
-//     import {
-//     MdButton,
-//     MdProgress,
-//     MdAvatar,
-//     MdCard,
-//     MdContent,
-//     MdElevation,
-//     MdIcon,
-//     MdImage,
-//     MdLayout,
-//     MdRipple,
-//     MdToolbar
-// } from "vue-material/dist/components";
-
-    // Vue.use(MdButton);
-    // Vue.use(MdProgress);
-    // Vue.use(MdAvatar);
-    // Vue.use(MdCard);
-    // Vue.use(MdContent);
-    // Vue.use(MdElevation);
-    // Vue.use(MdIcon);
-    // Vue.use(MdImage);
-    // Vue.use(MdLayout);
-    // Vue.use(MdRipple);
-    // Vue.use(MdToolbar);
 
     export default {
         data() {
