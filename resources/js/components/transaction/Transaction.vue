0285f28d54d841c682498410009aa6e488a8d219
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-27 15:01:04 +0000

    Redesigned Trans Page (TEMP. DESIGN)

--- resources/js/components/transaction/Transaction.vue
+++ resources/js/components/transaction/Transaction.vue
@@ -2,7 +2,7 @@
 <div class="bx--grid" style="padding: 40px 0px;" v-if="transaction">
     <div class="bx--row">
         <!-- Trans Details (Notfication Card) -->
-        <div class="bx--col-lg-6">
+        <div class="bx--col-lg-12">
             <div data-notification class="bx--inline-notification bx--inline-notification--info" role="alert">
                 <div class="bx--inline-notification__details">
                     <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--inline-notification__icon" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 5a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 7zm4 17.12h-8v-2.24h2.88v-6.76H13v-2.24h4.13v9H20z"></path></svg>
@@ -14,28 +14,24 @@
                                 <p class="bx--inline-notification__title">I.D: {{ transaction.id }}</p>
                                 <p class="bx--inline-notification__title">Project {{ assignment.title }}</p>
                                 <p class="bx--inline-notification__title">Transaction created at {{ transaction.created_at }}</p>
+                                <p class="bx--inline-notification__title">Transaction updated at {{ transaction.updated_at }}</p>
                                 <p class="bx--inline-notification__title">Amount €{{ transaction.amount }}</p>
+                                <p class="bx--inline-notification__title">Stripe I.D: {{ transaction.stripe_id }}</p>
                             </div>
                         </div>
                     </div>
                 </div>
+                
+                <!-- View Assignment Button -->
+                <cv-link :to="'/admin/assignments/show/' + transaction.assignment_id" style="text-decoration: none;">
+                    <button data-notification-btn class="bx--btn bx--btn--lg bx--btn--primary">
+                        View Project
+                    </button>
+                </cv-link>
             </div>
         </div>
     </div>
-
-
-
-
-
-
-        <div v-if="transaction">
-            <h1>Transaction</h1>
-            <p color="#fff">
-            {{transaction}}
-            </p>
-            <router-link :to="'/admin/assignments/show/' + transaction.assignment_id">View assignment</router-link>
-        </div>
-        <p v-else-if="error">{{error}}</p>
+        <!-- <p v-else="error">{{error}}</!--> 
 </div>
 </template>
 
