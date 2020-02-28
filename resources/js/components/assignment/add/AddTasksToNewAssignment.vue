c4ab40bb3d00f8c30cd6dbc9db019c23ef7b4c9d
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-28 12:58:44 +0000

    Attempted to fix AddTasksToNewAssignment (Failed Miserably lolololol)

--- resources/js/components/assignment/add/AddTasksToNewAssignment.vue
+++ resources/js/components/assignment/add/AddTasksToNewAssignment.vue
@@ -1,15 +1,70 @@
 <template>
 <div>
-    <ListTask />
+    <div v-if="saved">
+        <button 
+            @click="saved = false" 
+            class="bx--btn bx--btn--lg bx--btn--primary"
+            >
+                Select Again
+        </button>
 
+        <button
+            @click="submitTasks"
+            class="bx--btn bx--btn--lg bx--btn--primary"
+        >
+            Complete Project
+        </button>
+    </div>
 
+    <div v-else>
+        <div class="bx--data-table-container" data-table>
+            <!-- Title -->
+            <div class="bx--data-table-header">
+                <h4 class="bx--data-table-header__title">Tasks</h4>
+            </div>
 
+            <!-- Toolbar Contents -->
+            <section class="bx--table-toolbar">
+                <!-- Persistent Search -->
+                <div class="bx--toolbar-content">
+                    Add Task<CreateNewTaskForAssignment v-on:new-task="newTask"/>
+                </div>
+            </section>
 
+            <table v-if="tasks !== null" class="bx--data-table bx--data-table--sort">
+                <!-- Headings -->
+                <thead>
+                    <tr>
+                        <!-- Title -->
+                        <th>
+                            <button class="bx--table-sort" data-event="sort" title="title">
+                                <span class="bx--table-header-label">Title</span>
+                                <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--table-sort__icon" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true"><path d="M12.3 9.3l-3.8 3.8V1h-1v12.1L3.7 9.3 3 10l5 5 5-5z"></path></svg>
+                                <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--table-sort__icon-unsorted" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true"><path d="M13.8 10.3L12 12.1V2h-1v10.1l-1.8-1.8-.7.7 3 3 3-3zM4.5 2l-3 3 .7.7L4 3.9V14h1V3.9l1.8 1.8.7-.7z"></path></svg>
+                            </button>
+                        </th>
 
+                        <!-- Space for Add Task Buttons -->
+                        <th></th>
+                    </tr>
+                </thead>
 
+                <!-- Body -->
+                <tbody v-for="task in tasks" v-bind:key="task.id">
+                    <!-- Title -->
+                    <td>
+                        {{ task.title }}
+                    </td>
 
+                    <!-- Check Icon -->
+                    <Checkmark />
+                </tbody>
+            </table>
+            <p v-else>Select a Service</p>
+        </div>
+    </div>
 
-    <div class="fluid-container">
+    <!-- <div class="fluid-container">
         <div class="md-layout">
             <div class="md-layout-item">
                 <md-table-toolbar>
@@ -29,9 +84,9 @@
         </md-empty-state>
         <div v-else>
             <md-table class="background text" v-if="tasks" v-model="tasks" md-card @md-selected="onSelect">
-                <md-table-toolbar class="background text">
+                <md-table-toolbar class="background text"> -->
                     <!-- <h1 class="md-title">Select tasks</h1> -->
-                    <div class="md-toolbar-section-end background text">
+                    <!-- <div class="md-toolbar-section-end background text">
                         <span class="subject">Add Task</span><CreateNewTaskForAssignment v-on:new-task="newTask"/>
                     </div>
                 </md-table-toolbar>
@@ -51,7 +106,7 @@
             <p v-else>Select a Service</p>
         </div>
 
-    </div>
+    </div> -->
 </div>
 </template>
 <script>
