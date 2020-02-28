58933fec5e712d81c61750b8dbf6e72ef9dbe71f
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-02-28 13:19:40 +0000

    Clean up

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="bx--grid" style="margin: 80px 0px;">
-  <!-- Dashboard: <p v-if="user">{{msg}}, {{ user.first_name }}</p> -->
+  <!-- Dashboard: -->
   <div class="bx--row">
-    <!-- Client Details (Notification Card)-->
+    <!-- Client Details (Notification Card) -->
     <div class="bx--col-lg-6">
       <!-- Dropdown to choose Project -->
       <cv-dropdown name="assignment" id="assignment" class="cv-dropdown" v-model="assignmentId" placeholder="Select a Project" >
@@ -16,39 +16,32 @@
           <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--inline-notification__icon" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 5a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 7zm4 17.12h-8v-2.24h2.88v-6.76H13v-2.24h4.13v9H20z"></path></svg>
           
           <!-- Client Details Contents -->
-          <!-- <div class="bx--row"> -->
-            <div class="bx--inline-notification__text-wrapper">
-              <div class="bx--col-lg-12">
-                <p class="bx--inline-notification__title">{{ user.first_name }} {{ user.last_name }}</p>
-                <p class="bx--inline-notification__subtitle">{{ user.email }}</p>
-                <p class="bx--inline-notification__subtitle">{{ user.mobile_number }}</p>
-                <p class="bx--inline-notification__subtitle">{{ user.address }}</p>
-              </div>
+          <div class="bx--inline-notification__text-wrapper">
+            <div class="bx--col-lg-12">
+              <p class="bx--inline-notification__title">{{ user.first_name }} {{ user.last_name }}</p>
+              <p class="bx--inline-notification__subtitle">{{ user.email }}</p>
+              <p class="bx--inline-notification__subtitle">{{ user.mobile_number }}</p>
+              <p class="bx--inline-notification__subtitle">{{ user.address }}</p>
             </div>
           </div>
         </div>
-
-        <div class="assignment-container" v-if="assignmentId">
-          <!-- Progression Donut -->
-          <Chart v-bind:parentId="assignmentId" style="padding-left:100px;"/>
-        </div>
-
       </div>
 
-      <div class="bx--col-lg-6">
-        <!-- Active Task Datatable -->
-        <ListTask v-bind:parentId="assignmentId" v-bind:isActive="true" />
+      <div class="assignment-container" v-if="assignmentId">
+        <!-- Progression Donut -->
+        <Chart v-bind:parentId="assignmentId" style="padding-left:100px;"/>
       </div>
-  
-      <!-- Comments -->
-      <CommentTile v-bind:id="assignmentId" v-bind:isAssignment="true" />
-        
-
     </div>
-  </div>
 
+    <div class="bx--col-lg-6">
+      <!-- Active Task Datatable -->
+      <ListTask v-bind:parentId="assignmentId" v-bind:isActive="true" />
+    </div>
   
-
+    <!-- Comments -->
+    <CommentTile v-bind:id="assignmentId" v-bind:isAssignment="true" />
+  </div>
+</div>
 </template>
 
 <script>
