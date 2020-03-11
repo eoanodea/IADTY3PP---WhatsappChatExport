f66b2f0de1a218e3c885f25338f194e6eea024ff
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-03-11 14:29:32 +0000

    Minor redesign

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,27 +1,32 @@
 <template>
 <div class="bx--grid" style="margin: 80px 0px;">
-  <!-- Dashboard: -->
+  <div class="bx--row">
+    <!-- Dropdown to choose Project -->
+    <div class="bx--col-lg-12">
+      <cv-dropdown name="assignment" id="assignment" class="cv-dropdown" v-model="assignmentId" placeholder="Select a Project" >
+          <cv-dropdown-item class="warningBtn" v-for="(assignment, index) in assignments" v-bind:key="assignment.id" :value="assignment.id.toString()" :selected="index === 0">
+              Project {{assignment.title}} (ID: {{index}})
+          </cv-dropdown-item>
+      </cv-dropdown>
+    </div>
+  </div>
+
+  <br/>
+
   <div class="bx--row">
     <!-- Client Details (Notification Card) -->
     <div class="bx--col-lg-6">
-      <!-- Dropdown to choose Project -->
-      <cv-dropdown name="assignment" id="assignment" class="cv-dropdown" v-model="assignmentId" placeholder="Select a Project" >
-          <cv-dropdown-item class="warningBtn" v-for="(assignment, index) in assignments" v-bind:key="assignment.id" :value="assignment.id.toString()" :selected="index === 0">
-              {{index}}: {{assignment.title}}
-          </cv-dropdown-item>
-      </cv-dropdown>
-
       <div data-notification class="bx--inline-notification bx--inline-notification--info" role="alert">
         <div class="bx--inline-notification__details">
           <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--inline-notification__icon" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 5a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 7zm4 17.12h-8v-2.24h2.88v-6.76H13v-2.24h4.13v9H20z"></path></svg>
           
           <!-- Client Details Contents -->
           <div class="bx--inline-notification__text-wrapper">
-            <div class="bx--col-lg-12">
-              <p class="bx--inline-notification__title">{{ user.first_name }} {{ user.last_name }}</p>
-              <p class="bx--inline-notification__subtitle">{{ user.email }}</p>
-              <p class="bx--inline-notification__subtitle">{{ user.mobile_number }}</p>
-              <p class="bx--inline-notification__subtitle">{{ user.address }}</p>
+            <div class="bx--col">
+              <p class="bx--inline-notification__title" style="font-size: 30px;">{{ user.first_name }} {{ user.last_name }}</p><br/>
+              <p class="bx--inline-notification__subtitle tertiaryText">{{ user.email }}</p>
+              <p class="bx--inline-notification__subtitle tertiaryText">{{ user.mobile_number }}</p>
+              <p class="bx--inline-notification__subtitle tertiaryText">{{ user.address }}</p>
             </div>
           </div>
         </div>
