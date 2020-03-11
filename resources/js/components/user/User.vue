642f0f9f707ab8dfb595f33f4d29601929f82a91
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-03-11 12:31:23 +0000

    Repositioning

--- resources/js/components/user/User.vue
+++ resources/js/components/user/User.vue
@@ -1,83 +1,68 @@
 <template>
-  <loading-indicator v-if="loading"/>
-  <div class="bx--grid" v-else-if="user && user.id">
-    <div class="bx--row">
-      <!-- Client Details (Notification Card)-->
-      <div class="bx--col-lg-6">
-        <div data-notification class="bx--inline-notification bx--inline-notification--info" role="alert">
-          <div class="bx--inline-notification__details">
-            <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--inline-notification__icon" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 5a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 7zm4 17.12h-8v-2.24h2.88v-6.76H13v-2.24h4.13v9H20z"></path></svg>
-            
-            <!-- Client Details Contents -->
-            <div class="bx--row">
-              <div class="bx--inline-notification__text-wrapper">
-                <div class="bx--col-lg-12">
-                  <p class="bx--inline-notification__title">{{ user.first_name }} {{ user.last_name }}</p>
-                  <p class="bx--inline-notification__subtitle">{{ user.email }}</p>
-                  <p class="bx--inline-notification__subtitle">{{ user.mobile_number }}</p>
-                  <p class="bx--inline-notification__subtitle">{{ user.address }}</p>
-                </div>
-              </div>
+<div class="bx--grid">
+
+  <div class="bx--row">
+    
+    <!-- Client Details (Notification Card)-->
+    <div class="bx--col-lg-6">
+      
+      <div 
+        data-notification 
+        class="bx--inline-notification bx--inline-notification--info" 
+        role="alert"
+        >
+        <div class="bx--inline-notification__details">
+          <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--inline-notification__icon" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 5a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 7zm4 17.12h-8v-2.24h2.88v-6.76H13v-2.24h4.13v9H20z"></path></svg> 
+        
+          <!-- Client Details Contents -->
+          <div class="bx--inline-notification__text-wrapper">
+            <div class="bx--col-lg-12">
+              <p class="bx--inline-notification__title">{{ user.first_name }} {{ user.last_name }}</p>
+              <p class="bx--inline-notification__subtitle">{{ user.email }}</p>
+              <p class="bx--inline-notification__subtitle">{{ user.mobile_number }}</p>
+              <p class="bx--inline-notification__subtitle">{{ user.address }}</p>
             </div>
           </div>
-
-          <!-- Edit and Delete Button -->
-          <cv-link :to="'/admin/users/edit/' + user.id" style="text-decoration: none;">
-            <button data-notification-btn class="bx--btn bx--btn--lg bx--btn--primary">
-              Edit Client
-            </button>
-          </cv-link>
-          <DeleteUser style="text-decoration; none;" class="md-alignment-top-right" v-bind:id="user.id" />
-        </div>
-    </div>
-
-      <!-- Send Message -->
-      <div class="bx--col-lg-6">
-        <div class="bx--row">
-          <div class="bx--col-lg-12">
-              <div class="bx--form-item bx--text-input-wrapper">
-                  <label for="compose_message" class="bx--label"></label>
-                  <div class="bx--text-input__field-wrapper">
-                  <textarea 
-                      id="text-area-2" 
-                      name="compose_message" 
-                      type="text" 
-                      class="bx--text-area" 
-                      placeholder="Send a message"></textarea>
-                  </div>
-              </div>
-          </div>
         </div>
 
-        <!-- Send Message Button -->
-        <div class="bx--row">
-          <div class="bx--col-lg-12">
-            <!-- <cv-link> -->
-              <button class="bx--btn bx--btn--lg bx--btn--primary">
-                Send
-              </button>
-            <!-- </cv-link> -->
-          </div>
-        </div>
+        <!-- Edit Button -->
+        <cv-link :to="'/admin/users/edit/' + user.id" style="text-decoration: none;">
+          <button data-notification-btn class="bx--btn bx--btn--lg bx--btn--primary">
+            Edit Client
+          </button>
+        </cv-link>
+
+        <!-- Delete Modal Button -->        
+        <DeleteUser style="text-decoration; none;" class="md-alignment-top-right" v-bind:id="user.id" />
+        </div>        
       </div>
-    </div>
 
-    <!-- Progression Chart -->
-    <div class="assignment-container" v-if="assignmentId">
-      <!-- Progression Donut -->
-      <label 
-        for="compose_message" 
-        class="bx--label"
-        >Project Completion with {{ user.first_name }} {{ user.last_name }}
-      </label>
+      <!-- Chart -->
+      <div class="bx--col-lg-6">
+        <div class="assignment-container" v-if="assignmentId">
+          <label 
+            for="compose_message" 
+            class="bx--label"
+            >Project Completion with {{ user.first_name }} {{ user.last_name }}
+          </label>
 
-      <Chart v-bind:parentId="assignmentId" style="padding-left:100px;"/>
-    </div>
+          <Chart v-bind:parentId="assignmentId" style="padding-left:100px;"/>
+        </div>
+      </div> <!-- Close Chart COLUMN -->
+    
+    </div> <!-- Close Client Details Card COLUMN-->
 
-    <!-- Comments -->
-    <CommentTile v-bind:id="assignmentId" v-bind:isAssignment="true" />
-  </div> 
-  <data-error v-else v-bind:error="error" v-bind:collection="'user'" />
+  <!-- Comments -->
+  <CommentTile v-bind:id="assignmentId" v-bind:isAssignment="true" />
+
+  </div> <!-- Close Main ROW -->
+
+</div> <!-- Close GRID -->
+
+  <!-- <loading-indicator v-if="loading"/>
+  <div class="bx--grid" v-else-if="user && user.id">
+ 
+  <data-error v-else v-bind:error="error" v-bind:collection="'user'" /> -->
 </template>
 
 <script>
